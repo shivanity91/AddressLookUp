39d4f49dea277a2d57eaca6ca32efcc33e5c8ff7
Shivani Tyagi <shivani.tyagi@senecaglobal.com>
2022-07-17 13:31:47 +0530

    Removed extra files

--- Ping.API.old/Services/IPingLookUpService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Api.Common.Contracts;
-
-namespace Ping.Api.Services
-{
-    public interface IPingLookUpService
-    {
-        Task<PingLookUpResult> GetPingLookUpResultAsync(string address, CancellationToken cancellationToken = default);
-    }
-}
